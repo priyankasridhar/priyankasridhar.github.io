22261cafe9e743c042c12e1a216b28c62e15f0d6
anoop <gatoraze.gateway.2wire.net>
2013-11-11 20:15:55 -0800

    Committing initial stage changes
    adding images, reports, about page , see page work page

---
+++ scripts/main.js
@@ -0,0 +1,29 @@
+$(document).ready(function() {
+});
+
+
+function Report($scope){
+	$(".topbar").children(":eq(2)").addClass("active");
+	$scope.showReport1 = false;
+	$scope.showReport = function (num) {
+		$("#report-modal"+num).modal();
+	};
+	$scope.showImage = function (num) {
+		$("#metro6-image").attr("src", "images/metro6/photo"+num+".jpg");
+		$("#image-modal").modal();
+	};
+}
+
+function Home($scope){
+	$(".topbar").children(":eq(0)").addClass("active");
+}
+
+function Work($scope){
+	$(".topbar").children(":eq(3)").addClass("active");
+}
+function See($scope){
+	$(".topbar").children(":eq(1)").addClass("active");
+}
+function smartScroll(){
+
+}
